7daf2f32e43800efd3f64da3fd4aa4b787b97f83
melroylobo <34840682+melroylobo@users.noreply.github.com>
2024-08-01 20:08:28 +0530

    new layout

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          customColor: '#321323', // Replace with your custom color value
+          anotherColor: '#ff5733', // Example of another custom color
+        },
+      },
+    },
+    plugins: [],
+  }
